3261559de2bbb4328d3ba5fbb79701f30601aeb9
MathiasMeuleman <meuleman.mathias@gmail.com>
2020-12-23 14:15:43 +0100

    Add PdfLoader component

---
+++ packages/react-pdf-highlighter/src/PdfLoader.tsx
@@ -0,0 +1,96 @@
+
+import {getDocument, GlobalWorkerOptions, PDFDocumentProxy} from "pdfjs-dist";
+import React, {Component, ReactElement} from "react";
+// @ts-ignore-next-line
+import workerUrl from "pdfjs-dist/build/pdf.worker.js";
+
+// Set the PDF worker
+GlobalWorkerOptions.workerSrc = workerUrl;
+
+type PdfLoaderProps = {
+    url: string,
+    beforeLoad: ReactElement,
+    errorMessage?: ReactElement,
+    children: (pdfDocument: PDFDocumentProxy) => ReactElement,
+    onError?: (error: Error) => void,
+};
+
+type PdfLoaderState = {
+    pdfDocument?: PDFDocumentProxy,
+    error?: Error,
+};
+
+class PdfLoader extends Component<PdfLoaderProps, PdfLoaderState> {
+
+    documentRef = React.createRef<HTMLElement>();
+
+    discardDocument(document?: PDFDocumentProxy) {
+        document && document.destroy();
+    }
+
+    componentDidMount() {
+        this.load();
+    }
+
+    componentWillUnmount() {
+        this.discardDocument(this.state.pdfDocument);
+    }
+
+    componentDidUpdate({ url }: PdfLoaderProps) {
+        if (this.props.url !== url) {
+            this.load();
+        }
+    }
+
+    componentDidCatch(error: Error, info?: any) {
+        const { onError } = this.props;
+
+        if (onError) {
+            onError(error);
+        }
+
+        this.setState({ pdfDocument: undefined, error });
+    }
+
+    load() {
+        const { url } = this.props;
+        const { pdfDocument } = this.state;
+        this.setState({ pdfDocument: undefined, error: undefined });
+        try {
+            this.discardDocument(pdfDocument);
+            if (!url) return;
+            getDocument(url).promise.then(pdfDocument => {
+                this.setState({pdfDocument});
+            });
+        } catch (e) {
+            this.componentDidCatch(e);
+        }
+    }
+
+    render() {
+        const { children, beforeLoad } = this.props;
+        const { pdfDocument, error } = this.state;
+
+        return (
+            <>
+                <span ref={this.documentRef} />
+                {error
+                    ? this.renderError()
+                    : !pdfDocument || !children
+                        ? beforeLoad
+                        : children(pdfDocument)}
+            </>
+        );
+    }
+
+    renderError() {
+        const { errorMessage } = this.props;
+        if (errorMessage) {
+            return React.cloneElement(errorMessage, { error: this.state.error });
+        }
+
+        return null;
+    }
+}
+
+export default PdfLoader;
