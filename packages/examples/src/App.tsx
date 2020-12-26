9c5da8d79d48387b30ff5a681b03f30847f8fd2e
MathiasMeuleman <meuleman.mathias@gmail.com>
2020-12-26 12:25:00 +0100

    Use PdfViewer in example

--- packages/examples/src/App.tsx
+++ packages/examples/src/App.tsx
@@ -1,18 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ReactElement} from "react";
+import {PdfViewer} from "react-pdf-highlighter";
+import PdfLoader from "react-pdf-highlighter/dist/PdfLoader";
+import {BoundingRect, Content, Scaled, ScaledPosition, ViewportHighlight} from "react-pdf-highlighter/dist/types";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+
+    const highlightTransform = (
+        highlight: ViewportHighlight,
+        index: number,
+        setTip: (
+            highlight: ViewportHighlight,
+            callback: (highlight: ViewportHighlight) => ReactElement,
+        ) => void,
+        hideTip: () => void,
+        viewportToScaled: (rect: BoundingRect) => Scaled,
+        screenshot: (position: BoundingRect) => string,
+        isScrolledTo: boolean,
+    ) => {
+        console.log(highlight, index, setTip, hideTip, viewportToScaled, screenshot, isScrolledTo);
+        return <React.Fragment key={index}></React.Fragment>;
+    };
+
+    const onSelectionFinished = (
+        position: ScaledPosition,
+        content: Content,
+        hideTipAndSelection: () => void,
+        transformSelection: () => void,
+    ) => {
+        console.log(position, content, hideTipAndSelection, transformSelection);
+        return undefined;
+    };
+
+    return (
+        <div
+            style={{
+                width: "100vw",
+                height: "100vh",
+                position: "relative",
+                textAlign: "center",
+            }}
+        >
+            <PdfLoader url={"https://arxiv.org/pdf/1708.08021.pdf"} beforeLoad={<h1>Loading...</h1>}>
+                {(pdfDocument) => (
+                    <PdfViewer
+                        enableAreaSelection={(event: MouseEvent) => event.altKey}
+                        pdfDocument={pdfDocument}
+                        scrollRef={scrollTo => {
+                            console.log(scrollTo);
+                        }}
+                        onScrollChange={() => {
+                            console.log("Scroll changed");
+                        }}
+                        highlightTransform={highlightTransform}
+                        // onSelectionFinished={onSelectionFinished}
+                        highlights={[]}
+                    />
+                )}
+            </PdfLoader>
+        </div>
+    );
 }
 
 export default App;
